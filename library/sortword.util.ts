620260e88c15f2db78f81c7aba6cc0095a9b5cf7
kolynzb <collinsbenda360@gmail.com>
2023-06-20 14:21:46 +0300

    chore: make words lowercase

--- library/sortword.util.ts
+++ library/sortword.util.ts
@@ -2,11 +2,12 @@ import * as fs from 'fs';
 
 /**
  * Sorts the words in a text file in alphabetical order.
+ * Converts all words to lowercase.
  * @param filePath - The path to the text file.
  */
 function sortWordsInFile(filePath: string): void {
   const fileContent = fs.readFileSync(filePath, 'utf-8');
-  const words = fileContent.split('\n').filter(Boolean);
+  const words = fileContent.toLowerCase().split('\n').filter(Boolean);
   const sortedWords = words.sort();
   const sortedContent = sortedWords.join('\n');
   fs.writeFileSync(filePath, sortedContent);
@@ -16,4 +17,4 @@ function sortWordsInFile(filePath: string): void {
 const filePath = 'project-words.txt';
 sortWordsInFile(filePath);
 
-console.log('File successfully sorted!');
+console.log('File successfully sorted! All words are in lowercase.');
